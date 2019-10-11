02fb5bee51f45439cb6fcd99090159bc889a06b0
Ubuntu <ubuntu@ip-172-31-43-56.us-east-2.compute.internal>
2019-10-11 01:50:52 +0000

    Startup file updated

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 
 docker image build -t hotburger:v1 .
-docker container run -d -p 80:8080 --mount type=bind,source="$(pwd)"/project.log,target=/project.log hotburger:v1
+docker container run -d -p 8080:8080 --mount type=bind,source="$(pwd)"/project.log,target=/project.log hotburger:v1
